00445c1a43a8ca1e50b8aabb34c6d2618fbc3fdb
Vandam <mradziwilko@msn.com>
2015-11-28 17:10:39 -0400

    Fixed Overall Template, adding main Content next

---
+++ template/footer.php
@@ -0,0 +1,11 @@
+
+<div id="footer">
+		<div id="footerline"></div>
+    	<div id="CW">Copyright 2015/2016 Society Finder</div>
+    	<div id="helpicon"><img src="img/help.png" width="32" height="32" alt=""/></div>
+    </div>
+
+  </div>
+
+  </body>
+</html>
--- template/header.php
+++ template/header.php
@@ -28,14 +28,3 @@
                 <div id="txtbox"><input type="text" id="searchbartxt" placeholder="Please Enter a Search Topic."></div>
             </div>
             </div>
-
-<div id="footer">
-		<div id="footerline"></div>
-    	<div id="CW">Copyright 2015/2016 Society Finder</div>
-    	<div id="helpicon"><img src="img/help.png" width="32" height="32" alt=""/></div>
-    </div>
-
-  </div>
-
-  </body>
-</html>
